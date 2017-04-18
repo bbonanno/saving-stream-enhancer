846b064f5887c66bfdd3096e9ede35772f552bd0
Bruno Bonanno <bbonanno@gmail.com>
2017-04-18 12:12:45 +0100

    loan-portfolio version bump up

--- src/loan-portfolio.user.js
+++ src/loan-portfolio.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Loan Portfolio
 // @namespace    https://github.com/bbonanno/
-// @version      0.1.1
+// @version      0.2
 // @author       Bruno Bonanno
 // @match        https://lendy.co.uk/my-loans/live
 // @match        https://lendy.co.uk/loans/live
