f9009cd10d73bc4be380267f7d611a32ad56549d
Bruno Bonanno <bbonanno@gmail.com>
2017-04-21 11:40:58 +0100

    Fix improper use of arrow function

--- src/util.js
+++ src/util.js
@@ -1,14 +1,16 @@
-const NumericParser = (id, parser) => ({
-    id: id,
-    is: () => false,
-    format: s => {
-        if (s.trim().length > 0)
-            return parser(s);
-        else
-            return Number.MAX_SAFE_INTEGER;
-    },
-    type: 'numeric'
-});
+const NumericParser = function (id, parser) {
+    return {
+        id: id,
+        is: () => false,
+        format: s => {
+            if (s.trim().length > 0)
+                return parser(s);
+            else
+                return Number.MAX_SAFE_INTEGER;
+        },
+        type: 'numeric'
+    }
+};
 
 export const numberOfDays = s => parseInt(s.toLowerCase().replace('days', '').replace('day', '').trim());
 
